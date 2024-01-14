34833b6b8ff8b36f2e1e71bf5da04dc82fd78fed
samiul2102013 <samiulhasan0186@gmail.com>
2024-01-14 22:39:16 +0600

    first update

---
+++ app/src/main/java/comsamiul/example/database_project/HomePageActivity.java
@@ -0,0 +1,14 @@
+package comsamiul.example.database_project;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class HomePageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home_page);
+        // Additional initialization or logic for the home page
+    }
+}
---
+++ app/src/main/java/comsamiul/example/database_project/SignUpWithEmailActivity.java
@@ -0,0 +1,14 @@
+package comsamiul.example.database_project;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SignUpWithEmailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sing_up_with_email);
+        // Additional initialization or logic for the sign-up with email page
+    }
+}
